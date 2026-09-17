41e42f28113fa076177b43af1450d4e10e467656
SteudentGMI0507 <nurmaisarah.rosmadi@student.gmi.edu.my>
2026-09-17 20:11:30 +0800

    Add files via upload

---
+++ add.php
@@ -0,0 +1,87 @@
+<?php
+
+    $title = $email = $ingredients = '';
+    $errors = array('email'=>'', 'title'=>'', 'ingredients'=>'');
+
+    //this is where all the form stuff happens
+    if(isset($_POST['submit'])){
+
+        //check email
+        if(empty($_POST['email'])){
+            $errors['email'] = 'An email is required <br />';
+        } else{
+            $email = $_POST['email'];
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $errors['email'] = 'email must be a valid email address';
+            }
+        }
+        
+        //check title
+        if(empty($_POST['title'])){
+            $errors['title'] = 'A title is required <br />';
+        } else{
+            $title = $_POST['title'];
+            if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+                $errors['title'] = 'Title must be letter and spaces only';
+            }
+        }
+        
+        //check ingredients
+        if(empty($_POST['ingredients'])){
+            $errors['ingredients'] = 'At least one ingredient is required <br />';
+        } else{
+            $ingredients = $_POST['ingredients'];
+            if(!preg_match('/*([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+                $errors['ingredients'] = 'Ingredient must be a comma separated list';
+            }
+        }
+
+        if(array_filter($errors)){
+            //echo 'errors in the forms';
+        } else{
+            //echo 'form is valid';
+            header('Location: index.php');
+        }
+
+    } //end of POST check
+    
+    /*htmlspecialchars is a protection against doofuses who
+    wants to redirect you to a harmful website. So instead
+    of bringing you straight to the website, the link
+    entered in the form will just show back up as characters*/
+
+    //the conditionals are for data validation
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <?php include('templates/header.php') ?>
+
+    <!-- Form styling and whatnot-->
+    <section class="container grey-text">
+        <h4 class="center">Add a Pizza</h4>
+        <form action="add.php" class="white" method="GET">
+            <label>Your Email:</label>
+            <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+            <div class="red-text"><?php echo $errors['email']; ?></div>
+
+            <label>Pizza Title:</label>
+            <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+            <div class="red-text"><?php echo $errors['title']; ?></div>
+
+            <label>Ingredients (comma separated):</label>
+            <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
+            <div class="red-text"><?php echo $errors['ingredients']; ?></div>
+
+            <div class="center">
+                <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+            </div>
+        </form>
+    </section>
+
+    <?php include('templates/footer.php') ?>
+
+
+</html> 
